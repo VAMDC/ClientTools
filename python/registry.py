1794dbe01fb4facaf2a93ad50045e53f6818e0ef
Christian Endres <endres@ph1.uni-koeln.de>
2012-11-15 16:31:18 +0100

    correct registry.py

--- python/registry.py
+++ python/registry.py
@@ -8,12 +8,14 @@ routines for querying the registry
 
 REL_REG='http://registry.vamdc.eu/vamdc_registry/services/RegistryQueryv1_0'
 DEV_REG='http://casx019-zone1.ast.cam.ac.uk/registry/services/RegistryQueryv1_0'
+REL_REG='http://registry.vamdc.eu/registry-12.07/services/RegistryQueryv1_0'
+
 REGURL=DEV_REG
 WSDL=REGURL+'?wsdl'
 
 # this is a copy of the URL above but with
 # schema locations fixed:
-WSDL = 'http://tmy.se/t/devreg_wsdl.xml'
+#WSDL = 'http://tmy.se/t/devreg_wsdl.xml'
 
 from suds.client import Client
 from suds.xsd.doctor import Doctor
@@ -37,20 +39,32 @@ class RegistryDoctor(Doctor):
 def getNodeList():
 
     d = RegistryDoctor()
-    client = Client(WSDL,doctor=d)
+    client = Client(WSDL) #,doctor=d)
 
     qr="""declare namespace ri='http://www.ivoa.net/xml/RegistryInterface/v1.0';
-for $x in //ri:Resource
-where $x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']
-and $x/@status='active'
-return ($x/title, $x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']/interface/accessURL)"""
+<nodes>
+{
+   for $x in //ri:Resource
+   where $x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']
+   and $x/@status='active'
+   and $x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']/versionOfStandards='12.07'
+   return  <node><title>{$x/title/text()}</title><url>{$x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']/interface/accessURL/text()}</url></node>   
+}
+</nodes>"""
+
 
     v=client.service.XQuerySearch(qr)
     nameurls=[]
-    while v:
+    for node in v.node:
+        # take only the first url
+        try:
+            url = node.url.split(" ")[0]
+        except:
+            url = None
+            
 	nameurls.append({\
-			'name':v.pop(1),
-			'url':v.pop(0)['value'],
+			'name':node.title,
+    			'url':url,
 			})
     return nameurls
 
